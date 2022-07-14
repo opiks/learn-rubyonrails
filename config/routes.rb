400ffd6d0306a74cd4d35878f86fd69142645d4e
muhammad taufik zamaludin <taufikmuhammad550@gmail.com>
2022-07-14 14:15:33 +0700

    make register form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "main#index", as: 'home'
-  get "about", to: "about#index", as: 'about'
+  root to: "main#index", as: "home"
+  get "about", to: "about#index", as: "about"
+  get "sign-up", to: "registration#new", as: "registration"
+  post "sign-up", to: "registration#new", as: "registrationpost"
 end
